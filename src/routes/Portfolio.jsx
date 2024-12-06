9720ddb559b5a308d325c829f608b6b15b29ba9c
felipe bartolomeu <felipebart1999@gmail.com>
2024-12-05 21:48:06 -0300

    adicionei target blank

--- src/routes/Portfolio.jsx
+++ src/routes/Portfolio.jsx
@@ -10,12 +10,20 @@ const Portfolio = () => {
           Disney+ clone website with mobile adaptation, hosted on Vercel, using
           Gulp and SASS.
         </p>
-        <a href="https://clone-disneyplus-wtgy.vercel.app">
+        <a
+          href="https://clone-disneyplus-wtgy.vercel.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="\img\disneyPlusimg.png" alt="imagem 2" className="imgP" />
         </a>
       </div>
       <div className="estilo1">
-        <a href="https://nilotattoo.site">
+        <a
+          href="https://nilotattoo.site"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="\img\niloTattooimg.png" alt="imagem 1" className="imgP" />
         </a>
         <p>
@@ -33,12 +41,20 @@ const Portfolio = () => {
           design best practices to ensure a smooth and accessible user
           experience across devices.
         </p>
-        <a href="https://mvanuzapsicologa.com">
+        <a
+          href="https://mvanuzapsicologa.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="\img\vanuzaSiteimg.png" alt="imagem 2" className="imgP" />
         </a>
       </div>
       <div className="estilo1">
-        <a href="https://bart-english.vercel.app">
+        <a
+          href="https://bart-english.vercel.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img
             src="\img\EnglishCourseimg.png"
             alt="imagem 1"
@@ -52,12 +68,20 @@ const Portfolio = () => {
       </div>
       <div className="estilo1">
         <p>Website for a fictional hotel, built with Bootstrap.</p>
-        <a href="https://bart-hotel.vercel.app">
+        <a
+          href="https://bart-hotel.vercel.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="\img\BartHotelimg.png" alt="imagem 2" className="imgP" />
         </a>
       </div>
       <div className="estilo1">
-        <a href="https://todo-list-sooty-sigma-85.vercel.app">
+        <a
+          href="https://todo-list-sooty-sigma-85.vercel.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="/img/imgTodoList.png" alt="imagem 1" className="imgP" />
         </a>
         <p>
@@ -69,12 +93,20 @@ const Portfolio = () => {
         <p>
           Landing Page project made with HTML, CSS and JavaScript for GTA V.
         </p>
-        <a href="https://gta-v-mu.vercel.app">
+        <a
+          href="https://gta-v-mu.vercel.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="/img/imgGta.png" alt="imagem 2" className="imgP" />
         </a>
       </div>
       <div className="estilo1">
-        <a href="https://calculadora-react-eta-sable.vercel.app">
+        <a
+          href="https://calculadora-react-eta-sable.vercel.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="/img/imgCalculator.png" alt="imagem 3" className="imgP" />
         </a>
         <p>
@@ -91,7 +123,11 @@ const Portfolio = () => {
           it shows data on humidity, wind speed and current state of the sky.
           Project made with HTML, CSS and JS.
         </p>
-        <a href="https://api-clima-snowy.vercel.app">
+        <a
+          href="https://api-clima-snowy.vercel.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src="/img/imgApiClima.png" alt="imagem 4" className="imgP" />
         </a>
       </div>
