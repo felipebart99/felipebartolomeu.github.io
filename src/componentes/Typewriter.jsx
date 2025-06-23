63439d036e170ee562ea8073dbd179b8a38210d0
felipe bartolomeu <felipebart1999@gmail.com>
2025-06-22 21:02:30 -0300

    Typewriter

--- src/componentes/Typewriter.jsx
+++ src/componentes/Typewriter.jsx
@@ -1,28 +1,19 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-const Typewriter = ({ text, speed = 100, pause = 1000 }) => {
+const Typewriter = ({ text, speed = 100 }) => {
   const [index, setIndex] = useState(0);
   const [displayedText, setDisplayedText] = useState("");
 
   useEffect(() => {
-    let timeout;
-
     if (index < text.length) {
-      timeout = setTimeout(() => {
+      const timeout = setTimeout(() => {
         setDisplayedText((prev) => prev + text[index]);
         setIndex(index + 1);
       }, speed);
-    } else {
-      // Pausa e reinicia
-      timeout = setTimeout(() => {
-        setDisplayedText("");
-        setIndex(0);
-      }, pause);
+      return () => clearTimeout(timeout);
     }
-
-    return () => clearTimeout(timeout);
-  }, [index, text, speed, pause]);
+  }, [index, text, speed]);
 
   return <p>{displayedText}</p>;
 };
@@ -30,7 +21,6 @@ const Typewriter = ({ text, speed = 100, pause = 1000 }) => {
 Typewriter.propTypes = {
   text: PropTypes.string.isRequired,
   speed: PropTypes.number,
-  pause: PropTypes.number, // tempo de pausa antes de reiniciar
 };
 
 export default Typewriter;
