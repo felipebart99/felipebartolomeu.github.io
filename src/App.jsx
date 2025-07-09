3d6b0a2d3e17c7c2166ea2813f50649f01432e7e
felipe bartolomeu <felipebart1999@gmail.com>
2025-07-09 20:39:44 -0300

    upgrade imagesBg

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,19 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import NavBar from "./componentes/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    // Lista de todas as imagens de fundo usadas nas rotas
+    const backgroundImages = [`/img/spaceStars2.jpg`, `/img/spaceStars.webp`];
+
+    // Pré-carrega cada imagem
+    backgroundImages.forEach((imageUrl) => {
+      const img = new Image(); // Cria um novo elemento <img>
+      img.src = imageUrl; // Define o src para forçar o carregamento
+    });
+  }, []); // Executa apenas uma vez no carregamento inicial
   return (
     <div className="app">
       <header>
